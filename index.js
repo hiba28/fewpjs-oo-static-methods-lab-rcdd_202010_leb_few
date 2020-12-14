6fec02975632d0ade28d5fd0306d726f9e37f763
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-14 02:24:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class Formatter {
      let output = []
      for(let i = 0 ; i < arr.length ; i++)
       {
-        if(i === 0) output.push(this.capitalize)
+        if(i === 0) output.push(this.capitalize(arr[i]))
         else{
           if(arr[i] !== not[i])
             output.push(arr[i].charAt(0).toUpperCase() + arr[i].slice(1))
