1bd39d323e12241f4d697c1520c3f3b9b61ba158
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-14 02:01:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ class Formatter {
   static titleize (string){
      let not = ["the", "a"," an", "but", "of", "and", "for", "at", "by", "from"]
      let arr = string.split(" ")
+     arr[0].toUpperCase()
      for(let i = 0 ; i < arr.length ; i++)
       arr[i] !== not[i] ? arr[i].toUpperCase() : arr[i]
-     return arr
+     return arr.join("")
   }
 }
