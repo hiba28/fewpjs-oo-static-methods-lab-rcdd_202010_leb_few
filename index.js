dc4c40f071516b209c3698c7c67857c6e7d36987
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-14 01:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.slice(1)
   }
   static sanitize (string){
-    return
+    return string.replace(`[^A-Za-z0-9-']`)
   }
   static titleize (){
 
